95c02a861838b509f24e1a511c68177438868980
BrandonWatanabeStudentAccount <99722500+BrandonWatanabeStudentAccount@users.noreply.github.com>
2022-03-02 01:28:37 -0800

    Ran TokenSetup after editing token file

--- lexer/TokenType.java
+++ lexer/TokenType.java
@@ -6,9 +6,6 @@ package lexer;
  *  constants to their Symbols
 */
 
-//Utf16StringLit \u
-//TimestampLit \\d{4}~0[1-9]|1[012]~0[1-9]|[12][0-9]|3[01]~(2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]
-
 public class TokenType {
    public static java.util.HashMap<Tokens,Symbol> tokens = new java.util.HashMap<Tokens,Symbol>();
    public TokenType() {
