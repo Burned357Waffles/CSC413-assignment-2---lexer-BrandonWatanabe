511b72af350b6e43e195c2a84b98d1d92aa0a292
burned357waffles <burned357waffles@gmail.com>
2022-03-25 21:55:54 -0700

    testing commit

--- lexer/Lexer.java
+++ lexer/Lexer.java
@@ -283,7 +283,7 @@ public class Lexer {
           ch = source.read();
         } while (Character.isDigit(ch));
 
-        if (number.length() == 4) { //hello
+        if (number.length() == 4) { //hello there
           if (Character.toString(ch).equals("~"))
           {
             endPosition++;
